e8fb36ca09803837f8fd65652fcde527ec45aac6
student-wroclaw <pi.imbor@gmail.com>
2022-12-11 13:49:30 +0100

    "FizzBuzzv2"

--- FizzBuzz/the_app.py
+++ FizzBuzz/the_app.py
@@ -1,8 +1,25 @@
 def fizzbuzz(number):
+    try: int(number)
+    except ValueError:
+        return 'blad'
+
+    else:
+        a = number % 3
+        b = number % 5
+        if number <= 0:
+            return None
+        elif a == 0 and b == 0:
+            return ('FizzBuzz')
+        elif b == 0:
+            return ('Buzz')
+        elif a == 0:
+            return ('Fizz')
+        else:
+            return (number)
+
+
     a = int(number) % 3
     b = int(number) % 5
-
-
     if a == 0 and b == 0:
         return('FizzBuzz')
     elif b == 0:
--- FizzBuzz/the_test.py
+++ FizzBuzz/the_test.py
@@ -4,4 +4,9 @@ def test_0():
     assert fizzbuzz(1) == 1
     assert fizzbuzz(2) == 2
     assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(15) == 'FizzBuzz'
+    assert fizzbuzz(0) == None
 
+
+def test_1():
+    assert fizzbuzz('kot') == 'blad'
